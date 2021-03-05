3a29865310db7ee220147b111be50c805fe34cae
SeokMin <lsm@unison.co.kr>
2021-03-05 10:23:17 +0900

    chapter03_04.py

--- chapter03_04.py
+++ chapter03_04.py
@@ -4,29 +4,30 @@
 # 튜플 자료형(순서o, 중복o, 수정x, 삭제x) # 불변
 
 # 선언
+
 a = ()
-b = (1, ) # 콤마를 찍어야 tuple로 인식
+b = (1,)
 c = (11, 12, 13, 14)
-d = (100, 1000, 'Ace', 'Base', 'Captain')
-e = (100, 1000, ('Ace', 'Base', 'Captain'))
+d = (100, 1000, 'Ace', 'Base', 'Captaine')
+e = (100, 1000, ('Ace', 'Base', 'Captaine'))
+
 
 # 인덱싱
 print('>>>>>')
 print('d - ', d[1])
 print('d - ', d[0] + d[1] + d[1])
 print('d - ', d[-1])
-print('d - ', e[-1])
-print('d - ', e[-1][1])
-print('d - ', list(e[-1][1]))
+print('e - ', e[-1])
+print('e - ', list(e[-1][1]))
 
 # 수정x
-#d[0] = 1500
+# d[0] = 1500
 
 # 슬라이싱
 print('>>>>>')
 print('d - ', d[0:3])
 print('d - ', d[2:])
-print('e - ', e[2][1:3])
+print('d - ', e[2][1:3])
 
 # 튜플 연산
 print('>>>>>')
@@ -43,9 +44,10 @@ print('a - ', a.count(2))
 
 # 팩킹
 t = ('foo', 'bar', 'baz', 'qux')
+
 print(t)
 print(t[0])
-print(t[1])
+print(t[-1])
 
 # 언팩킹1
 (x1, x2, x3, x4) = t
