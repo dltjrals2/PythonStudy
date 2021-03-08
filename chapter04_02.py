d2c83a3a2b469afa9e1576d59eaf7dd77f640e9a
SeokMin <lsm@unison.co.kr>
2021-03-08 12:43:15 +0900

    chapter04_02.py

--- chapter04_02.py
+++ chapter04_02.py
@@ -47,3 +47,78 @@ for n in names:
     print('You are : ', n)
 
 # 예제2
+lotto_numbers = [11, 19, 21, 28, 36, 37]
+
+for n in lotto_numbers:
+    print("Current number : ", n)
+
+# 예제3
+word = "Beautiful"
+
+for s in word:
+    print('word : ', s)
+
+# 예제4
+my_info = {
+    "name": 'Lee',
+    "Age": 33,
+    "City": "Seoul",
+}
+
+for key in my_info:
+    print('key : ', my_info[key])
+
+for v in my_info.values():
+    print('value : ', v)
+
+# 예제5
+name = 'FineAppLe'
+
+for n in name:
+    if n.isupper():
+        print(n)
+    else:
+        print(n.upper())
+
+# break
+numbers = [14, 3, 4, 7, 10, 24, 17, 2, 33, 15, 34, 36, 38]
+
+for num in numbers:
+    if num == 34:
+        print('Found : 34!')
+        break
+    else:
+        print('Not found : ', num)
+
+# continue
+lt = ["1", 2, 5, True, 4.3, complex(4)]
+
+for v in lt:
+    if type(v) is bool:
+        continue
+    print("Current type : ", v, type(v))
+    print("multiply by 2", v * 3)
+
+# for ~ else
+numbers = [14, 3, 4, 7, 10, 24, 17, 2, 33, 15, 34, 36, 38]
+
+for num in numbers:
+    if num == 24:
+        print("Found : 24")
+        break
+else:
+    print("Not Found : 24")
+
+# 구구단
+for i in range(2, 10):
+    for j in range(1, 10):
+        print('{:4d}'.format(i*j), end='')
+    print()
+
+# 변환 예제
+name2 = 'Aceman'
+
+print('Reversed', reversed(name2))
+print('List', list(reversed(name2)))
+print('Tuple', tuple(reversed(name2)))
+print('Set', set(reversed(name2))) # 순서x
